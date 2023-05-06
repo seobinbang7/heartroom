e393556b1a14bf963af6b0aa7033c975b9ffec63
방서빈 <newmso5382@gmail.com>
2023-05-06 12:34:05 +0900

    Add: package 파일 & index.html & star.jpg 추가 & Router.js 추가

---
+++ heartroom/src/Router.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main/Main";
+
+const Router = () => {
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />}></Route>
+    </Routes>
+  </BrowserRouter>;
+};
+
+export default Router;
