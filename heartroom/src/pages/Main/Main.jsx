9084fa56a980efc367dec960a89e4270904dd96f
방서빈 <newmso5382@gmail.com>
2023-05-08 23:51:26 +0900

    Add: 책갈피 추가 완료

--- heartroom/src/pages/Main/Main.jsx
+++ heartroom/src/pages/Main/Main.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import Music from "../Music/Music";
+// import Chat from "../Chat/Chat"
 import "./Main.css";
 
 export default function Main() {
   return (
     <>
-      <header></header>
+      <header>{/* <Music /> */}</header>
+      <div className="bookmark">
+        <ul className="bookmark__ul">
+          <li className="bookmark__li">
+            <a>준비중</a>
+          </li>
+          <li className="bookmark__li">
+            <a>준비중</a>
+          </li>
+          <li className="bookmark__li">
+            <a>준비중</a>
+          </li>
+        </ul>
+      </div>
       <main>
         <FontAwesomeIcon icon={faHeart} className="heart animate-heartbeat" />
-        
-        <div>
-          <a href="#" className="bookmark"></a>
-          <a href="#" className="bookmark"></a>
-          <a href="#" className="bookmark"></a>
-        </div>
+        {/* <Chat /> */}
       </main>
     </>
   );
